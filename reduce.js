3850fd87de149c4ef3f6ea9948b0643294d858bf
unknown <najimuddinhelalal670@gmail.com>
2025-09-11 06:32:59 +0430

    reduce method

---
+++ reduce.js
@@ -0,0 +1,26 @@
+// const numbers = [1, 2, 3, 4, 5];
+
+// const sum = numbers.reduce((acc, current) => acc + current, 0);
+
+// console.log(sum);
+
+// Find Maximum
+// const numbers = [3, 7, 2, 9, 5];
+
+// const max = numbers.reduce((acc, current) => {
+//   return current > acc ? current : acc;
+// }, numbers[0]);
+
+// console.log(max); // 9
+
+// Count items
+
+const fruits = ["apple", "banana", "apple", "orange", "banana", "apple"];
+
+const count = fruits.reduce((acc, fruit) => {
+  acc[fruit] = (acc[fruit] || 0) + 1;
+  return acc;
+}, {});
+
+console.log(count);
+// { apple: 3, banana: 2, orange: 1 }
