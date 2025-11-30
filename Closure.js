04f606dbd112de463af28b6797841d14bda1cfa3
unknown <najimuddinhelalal670@gmail.com>
2025-11-30 23:42:56 +0600

    added bankAccount function

--- Closure.js
+++ Closure.js
@@ -1,14 +1,33 @@
-function outer() {
-  let count = 0;
+// function outer() {
+//   let count = 0;
 
-  function inner() {
-    count++;
-    console.log(count);
-  }
+//   function inner() {
+//     count++;
+//     console.log(count);
+//   }
 
-  return inner;
+//   return inner;
+// }
+
+// const counter = outer();
+// counter();
+// counter();
+
+function bankAccount() {
+  let balance = 0;
+
+  return {
+    deposit(amount) {
+      balance += amount;
+      console.log("Balance:", balance);
+    },
+    withdraw(amount) {
+      balance -= amount;
+      console.log("Balance:", balance);
+    },
+  };
 }
 
-const counter = outer();
-counter(); // 1
-counter(); // 2
+const account = bankAccount();
+account.deposit(100);
+account.withdraw(20);
