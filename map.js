5ca036dd123ff0d46d87bdffb6ff305abf13e02a
unknown <najimuddinhelalal670@gmail.com>
2025-12-02 23:50:44 +0600

    added maping part-3

--- map.js
+++ map.js
@@ -1,5 +1,15 @@
-const numbers = [1, 2, 3, 4];
+// const numbers = [1, 2, 3, 4];
 
-const double = numbers.map((num) => num * 2);
+// const double = numbers.map((num) => num * 2);
 
-console.log(double);
+// console.log(double);
+
+// const names = ["najim", "rahim", "karim"];
+// const upperNames = names.map((name) => {
+//   return name.toUpperCase();
+// });
+// console.log(upperNames);
+
+const names = ["najim", "rahim", "karim"];
+const upperNames = names.map((name) => name.toUpperCase());
+console.log(upperNames);
