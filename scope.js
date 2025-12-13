d01423ef827506e65448bdebaa41ab8fccadec24
unknown <najimuddinhelalal670@gmail.com>
2025-12-13 23:42:49 +0600

    added advance clouser

--- scope.js
+++ scope.js
@@ -13,31 +13,57 @@
 //   var myClass = "Degree 3rd year";   //← NOT block-scoped
 // }
 
-console.log(name);
-console.log(age);
-console.log(myClass); //output =>Degree 3rd year ✔🟢
+// console.log(name);
+// console.log(age);
+// console.log(myClass);
 
 // Lexical scope
 
-function outer() {
-  let outerVar = "I am a student";
-  function inner() {
-    console.log(outerVar);
-  }
-  inner();
-}
+// function outer() {
+//   let outerVar = "I am a student";
+//   function inner() {
+//     console.log(outerVar);
+//   }
+//   inner();
+// }
 
-outer();
+// outer();
 
-// Closurse scope
-function makeCounter() {
-  let count = 0;
-  return function () {
-    count++;
-    console.log(count);
+// // Closurse scope
+// function makeCounter() {
+//   let count = 0;
+//   return function () {
+//     count++;
+//     console.log(count);
+//   };
+// }
+
+// const counter = makeCounter();
+// counter();
+// counter();
+
+function createBankAccount(ownerName) {
+  let balance = 0;
+
+  return {
+    deposit(amount) {
+      balance += amount;
+      console.log(`${ownerName} balance: ${balance}`);
+    },
+
+    withdraw(amount) {
+      if (amount > balance) {
+        console.log("Not enough balance");
+      } else {
+        balance -= amount;
+        console.log(`${ownerName} balance: ${balance}`);
+      }
+    },
   };
 }
 
-const counter = makeCounter();
-counter();
-counter();
+const najimAccount = createBankAccount("Najim");
+
+najimAccount.deposit(500);
+najimAccount.withdraw(200);
+najimAccount.withdraw(500);
