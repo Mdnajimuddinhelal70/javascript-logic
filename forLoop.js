38f3383a00a15183ac0f8d6d89f324f0d7ece8f1
unknown <najimuddinhelalal670@gmail.com>
2025-12-15 23:45:26 +0600

    added for in loop

--- forLoop.js
+++ forLoop.js
@@ -1,5 +1,15 @@
-const names = ["Rahim", "Karim", "Hasan"];
+// const names = ["Rahim", "Karim", "Hasan"];
 
-for (let i = 0; i < names.length; i++) {
-  console.log(names[i]);
+// for (let i = 0; i < names.length; i++) {
+//   console.log(names[i]);
+// }
+
+const student = {
+  name: "Najim",
+  age: 20,
+  role: "Developer",
+};
+
+for (let key in student) {
+  console.log(key, student[key]);
 }
