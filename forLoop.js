910f7abca9dc88e39395b998cb8ceb522afbb730
unknown <najimuddinhelalal670@gmail.com>
2026-01-12 23:26:15 +0600

    for loop with number

--- forLoop.js
+++ forLoop.js
@@ -4,12 +4,18 @@
 //   console.log(names[i]);
 // }
 
-const student = {
-  name: "Najim",
-  age: 20,
-  role: "Developer",
-};
+// const student = {
+//   name: "Najim",
+//   age: 20,
+//   role: "Developer",
+// };
 
-for (let key in student) {
-  console.log(key, student[key]);
+// for (let key in student) {
+//   console.log(key, student[key]);
+// }
+
+const numbers = [10, 20, 30];
+
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
 }
