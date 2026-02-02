88e995f9f6861b29d29376093569cc6d82b2c430
unknown <najimuddinhelalal670@gmail.com>
2026-02-02 14:52:19 +0600

    Numbers from 1 to 5, one missing

--- map.js
+++ map.js
@@ -18,18 +18,31 @@
 // console.log(upperNames);
 
 //Find the second largest number in an array
-const numbers = [10, 45, 2, 99, 23];
+// const numbers = [10, 45, 2, 99, 23];
 
-let max = -Infinity;
-let secondMax = -Infinity;
+// let max = -Infinity;
+// let secondMax = -Infinity;
 
-for (let num of numbers) {
-  if (num > max) {
-    secondMax = max;
-    max = num;
-  } else if (num > secondMax && num !== max) {
-    secondMax = num;
-  }
+// for (let num of numbers) {
+//   if (num > max) {
+//     secondMax = max;
+//     max = num;
+//   } else if (num > secondMax && num !== max) {
+//     secondMax = num;
+//   }
+// }
+
+// console.log(secondMax);
+
+// Numbers from 1 to 5, one missing
+const arr = [1, 2, 4, 5];
+
+const n = 5;
+const expectedSum = (n * (n + 1)) / 2;
+
+let actualSum = 0;
+for (let num of arr) {
+  actualSum += num;
 }
 
-console.log(secondMax);
+console.log(expectedSum - actualSum);
