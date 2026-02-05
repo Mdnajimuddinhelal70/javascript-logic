0f59941ce5de9d18b138f4856f14744ee36b34ce
unknown <najimuddinhelalal670@gmail.com>
2026-02-05 23:53:35 +0600

    Sum of numbers from 1 to 10 using while loop

--- whileLoop.js
+++ whileLoop.js
@@ -1,12 +1,23 @@
-let i = 1;
-while (i <= 5) {
-  console.log("Number: " + i);
-  i++;
+// let i = 1;
+// while (i <= 5) {
+//   console.log("Number: " + i);
+//   i++;
+// }
+
+// // For of loop
+// const fruits = ["Apple", "Banana", "Mango"];
+
+// for (const fruit of fruits) {
+//   console.log(fruit);
+// }
+
+// Sum of numbers from 1 to 10 using while loop
+let total = 0;
+let number = 1;
+
+while (number <= 10) {
+  total = total + number;
+  number++;
 }
 
-// For of loop
-const fruits = ["Apple", "Banana", "Mango"];
-
-for (const fruit of fruits) {
-  console.log(fruit);
-}
+console.log("Sum 1 to 10 =", total);
