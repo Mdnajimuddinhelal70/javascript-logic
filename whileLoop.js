b1236ae1fd89f8d1f937c36a99995965bc2693d1
unknown <najimuddinhelalal670@gmail.com>
2026-02-05 23:56:51 +0600

    Factorial of a number using while loop

--- whileLoop.js
+++ whileLoop.js
@@ -12,12 +12,23 @@
 // }
 
 // Sum of numbers from 1 to 10 using while loop
-let total = 0;
-let number = 1;
+// let total = 0;
+// let number = 1;
 
-while (number <= 10) {
-  total = total + number;
-  number++;
+// while (number <= 10) {
+//   total = total + number;
+//   number++;
+// }
+
+// console.log("Sum 1 to 10 =", total);
+
+// Factorial of a number using while loop
+let number = 5;
+let factorial = 1;
+
+while (number > 0) {
+  factorial = factorial * number;
+  number--;
 }
 
-console.log("Sum 1 to 10 =", total);
+console.log("Factorial =", factorial);
